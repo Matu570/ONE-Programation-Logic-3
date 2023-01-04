d4cd91857a512ce793f7415aa938bee53dd317e0
Matias Bueno <matiasbueno@hotmail.com.ar>
2023-01-04 12:41:02 -0300

    Making logos to the flag

--- Canvas/script.js
+++ Canvas/script.js
@@ -2,12 +2,41 @@ let canvas=document.querySelector("canvas");
 let brush=canvas.getContext("2d");
 brush.fillStyle="lightgrey";
 brush.fillRect(0,0,600,400);
+brush.strokeStyle = "#FF0000";
 
 brush.fillStyle="green";
 brush.fillRect(0,0,200,400);
 
-brush.fillStyle="yellow";
+brush.fillStyle="white";
 brush.fillRect(200,0,200,400);
 
 brush.fillStyle="red";
 brush.fillRect(400,0,200,400);
+
+brush.fillStyle="black";
+brush.beginPath();
+brush.moveTo(200, 150);
+brush.lineTo(400, 250);
+brush.lineTo(400, 260);
+brush.lineTo(200, 160);
+brush.lineTo(200, 150);
+brush.fill();
+
+brush.fillStyle="black";
+brush.beginPath();
+brush.moveTo(400, 150);
+brush.lineTo(200, 250);
+brush.lineTo(200, 260);
+brush.lineTo(400, 160);
+brush.lineTo(400, 150);
+brush.fill();
+
+brush.fillStyle="#FF00AA";
+brush.beginPath();
+brush.arc(300,100,65,0,2*3.14);
+brush.fill();
+
+
+
+
+
