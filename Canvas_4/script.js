3866e807035618658a4c8fa6b4e5bea181f265fd
Matias Bueno <matiasbueno@hotmail.com.ar>
2023-01-22 23:50:57 -0300

    Animation finished

--- Canvas_4/script.js
+++ Canvas_4/script.js
@@ -3,15 +3,38 @@ let brush = canvas.getContext("2d");
 
 brush.fillStyle = "lightgrey";
 brush.fillRect(0, 0, 600, 400);
+let x = 20;
+let moveAlong = true;
 
 function drawCircles(x, y, radio) {
     brush.fillStyle = "blue";
     brush.beginPath();
     brush.arc(x, y, radio, 0, 2 * Math.PI);
     brush.fill();
+    // if (moveAlong) {
+    //     if (x < 580) {
+    //         x+=20;
+    //     }else{moveAlong=false;}
+    // }else if (moveAlong==false) {
+    //     if (x>20){
+    //         x-=20;
+    //     }else{moveAlong=true};
+    // };
+}
+
+function cleanScreen() {
+    brush.clearRect(0, 0, 600, 400);
 };
 
-for (let index = 20; index < 600; index++) {
-    brush.clearRect(0, 0, 600, 400)
-    drawCircles(index, 20, 10);
+function refreshScreen() {
+    cleanScreen();
+    drawCircles(x, 20, 20);
+    if(x<580 && moveAlong){
+        x++;
+    }else{moveAlong=false};
+    if(moveAlong==false && x>20){
+        x--;
+    }else{moveAlong=true;}
 };
+
+setInterval(refreshScreen, 2);
